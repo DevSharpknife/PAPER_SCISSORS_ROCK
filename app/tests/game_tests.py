d917822733d3ae12c6b0d62fd64b9847b82f60c9
DevSharpknife <davidjamesmurphy@gmail.com>
2020-09-26 23:25:02 +0100

    edited test as I was referring to choice instead of player and added more options of games and players

--- app/tests/game_tests.py
+++ app/tests/game_tests.py
@@ -5,9 +5,21 @@ from app.models.player import Player
 class TestGame(unittest.TestCase):
 
     def setUp(self):
-            self.player_1 = Player("Dave", "Rock")
-            self.player_2 = Player("Other Dave", "Paper")
+            self.player_1 = Player("Maeve", "Rock")
+            self.player_2 = Player("Dolores", "Paper")
+            self.player_3 = Player("Bernard", "Scissors")
+            self.player_4 = Player("William", "Scissors")
             self.game = Game(self.player_1, self.player_2)
+            self.game_2 = Game(self.player_1, self.player_3)
+            self.game_3 = Game(self.player_3, self.player_4)
 
-    def test_battle(self):
-        self.assertEqual("Other Dave Wins!", self.game.battle(self.player_1.choice, self.player_2.choice))
+    def test_battle__P1_win(self):
+        self.assertEqual("Dolores Wins!", self.game.battle(self.player_1, self.player_2))
+    
+    def test_battle__P1_lose(self):
+        self.assertEqual("Maeve Wins!", self.game_2.battle(self.player_1, self.player_3))
+    
+    def test_battle__draw(self):
+        self.assertEqual("DRAW!", self.game_3.battle(self.player_3, self.player_4))
+
+    
