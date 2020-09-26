38443bc0cac377cecac343656adfcca45a483945
DevSharpknife <davidjamesmurphy@gmail.com>
2020-09-27 00:40:46 +0100

    FUrther editing done to ensure tests passed

--- app/tests/game_tests.py
+++ app/tests/game_tests.py
@@ -2,24 +2,23 @@ import unittest
 from app.models.game import Game
 from app.models.player import Player
 
+
 class TestGame(unittest.TestCase):
 
-    def setUp(self):
-            self.player_1 = Player("Maeve", "Rock")
-            self.player_2 = Player("Dolores", "Paper")
-            self.player_3 = Player("Bernard", "Scissors")
-            self.player_4 = Player("William", "Scissors")
-            self.game = Game(self.player_1, self.player_2)
-            self.game_2 = Game(self.player_1, self.player_3)
-            self.game_3 = Game(self.player_3, self.player_4)
-
     def test_battle__P1_win(self):
-        self.assertEqual("Dolores Wins!", self.game.battle(self.player_1, self.player_2))
+        self.P1 = Player("Maeve", "Rock")
+        self.P2 = Player("Dolores", "Paper")
+        self.game_1 = Game(self.P2, self.P1)
+        self.assertEqual("Dolores Wins!", self.game_1.battle(self.P1, self.P2))
     
     def test_battle__P1_lose(self):
-        self.assertEqual("Maeve Wins!", self.game_2.battle(self.player_1, self.player_3))
+        self.P1 = Player("Maeve", "Rock")
+        self.P3 = Player("Bernard", "Scissors")
+        self.game_2 = Game(self.P1, self.P3)
+        self.assertEqual("Maeve Wins!", self.game_2.battle(self.P1, self.P3))
     
     def test_battle__draw(self):
-        self.assertEqual("DRAW!", self.game_3.battle(self.player_3, self.player_4))
-
-    
+        self.P3 = Player("Bernard", "Scissors")
+        self.P4 = Player("William", "Scissors")
+        self.game_3 = Game(self.P3, self.P4) 
+        self.assertEqual("DRAW!", self.game_3.battle(self.P3, self.P4))
