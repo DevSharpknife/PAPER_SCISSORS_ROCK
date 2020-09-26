711d15e6ddfe0ebbc30ed9ce79866cf866ce818d
DevSharpknife <davidjamesmurphy@gmail.com>
2020-09-26 20:33:42 +0100

    Got logic working for 1st test

--- app/models/game.py
+++ app/models/game.py
@@ -5,19 +5,19 @@ class Game():
         self.player_2 = player_2
 
     def battle(self, choice_1, choice_2):
-        if player_1.choice == player_2.choice:
+        if self.player_1.choice == self.player_2.choice:
             return "DRAW!"
-        elif player_1.choice == "Paper" and player_2.choice == "Scissors":
-            return f"{player_2.name} Wins!"
-        elif player_1.choice == "Paper" and player_2.choice == "Rock":
-            return f"{player_1.name} Wins!"
-        elif player_1.choice == "Scissors" and player_2.choice == "Paper":
-            return f"{player_1.name} Wins!"
-        elif player_1.choice == "Scissors" and player_2.choice == "Rock":
-            return f"{player_2.name} Wins!"
-        elif player_1.choice == "Rock" and player_2.choice == "Paper":
-            return f"{player_2.name} Wins!"
-        elif player_1.choice == "Rock" and player_2.choice == "Scissors":
-            return f"{player_1.name} Wins!"
+        elif self.player_1.choice == "Paper" and self.player_2.choice == "Scissors":
+            return f"{self.player_2.handle} Wins!"
+        elif self.player_1.choice == "Paper" and self.player_2.choice == "Rock":
+            return f"{self.player_1.handle} Wins!"
+        elif self.player_1.choice == "Scissors" and self.player_2.choice == "Paper":
+            return f"{self.player_1.handle} Wins!"
+        elif self.player_1.choice == "Scissors" and self.player_2.choice == "Rock":
+            return f"{self.player_2.handle} Wins!"
+        elif self.player_1.choice == "Rock" and self.player_2.choice == "Paper":
+            return f"{self.player_2.handle} Wins!"
+        elif self.player_1.choice == "Rock" and self.player_2.choice == "Scissors":
+            return f"{self.player_1.handle} Wins!"
         else:
             return "DOES NOT COMPUTE!!!!"
