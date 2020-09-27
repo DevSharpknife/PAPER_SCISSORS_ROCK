68e9a8dfac04d4eac9faa0a8f7649797cd5a8a4c
DevSharpknife <davidjamesmurphy@gmail.com>
2020-09-27 17:55:59 +0100

    Welcome Page added with brief history of PSR and some other refactoring done

--- app/controller.py
+++ app/controller.py
@@ -1,11 +1,15 @@
 from flask import render_template, request, redirect
 from app import app
-#! from app.models.game import battle
-#! from app.models.player import *
+from app.models.game import *
+from app.models.player import *
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Home')
+    return render_template('index.html', title='A WORLD OF PAIN!!!')
 
-#! @app.route('/<choice_1>/<choice_2>', methods=['POST'])
-#! def play_rps():
+@app.route('/battle')
+def battle():
+    return render_template('battle.html', title='READY FOR BATTLE?')
+
+# @app.route('/<choice_1>/<choice_2>', methods=['POST'])
+# def play_rps():
