2282a5a0966e6131899a29b25e075cb5b0da361b
DevSharpknife <davidjamesmurphy@gmail.com>
2020-09-27 02:13:54 +0100

    Editing html to include form to make python logic more usable

--- app/controller.py
+++ app/controller.py
@@ -1,10 +1,11 @@
-from flask import render_template, request
+from flask import render_template, request, redirect
 from app import app
+#! from app.models.game import battle
+#! from app.models.player import *
 
 @app.route('/')
 def index():
     return render_template('index.html', title='Home')
 
-# @app.route('/<choice_1>/<choice_2>', methods=['POST'])
-# def play_rps():
-    
+#! @app.route('/<choice_1>/<choice_2>', methods=['POST'])
+#! def play_rps():
